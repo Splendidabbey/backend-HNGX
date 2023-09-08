7bbc77d363b3a7972d977b571054a176af909ed4
Abiodun Olorunda <34971027+Splendidabbey@users.noreply.github.com>
2023-09-08 14:23:46 +0100

    Added file via upload

---
+++ api.php
@@ -0,0 +1,31 @@
+<?php
+// Parse GET parameters
+$slackName = isset($_GET['slack_name']) ? $_GET['slack_name'] : "";
+$track = isset($_GET['track']) ? $_GET['track'] : "";
+
+// Get the current day of the week
+$currentDay = date("l");
+
+// Get current UTC time
+$utcTime = gmdate("Y-m-d\TH:i:s\Z");
+
+// Create the response array
+$response = [
+    "slack_name" => $slackName,
+    "current_day" => $currentDay,
+    "utc_time" => $utcTime,
+    "track" => $track,
+    "github_file_url" => "https://github.com/username/repo/blob/main/file_name.ext",
+    "github_repo_url" => "https://github.com/username/repo",
+    "status_code" => 200
+];
+
+// Encode the response as JSON
+$jsonResponse = json_encode($response);
+
+// Set the response content type to JSON
+header("Content-Type: application/json");
+
+// Return the JSON response
+echo $jsonResponse;
+?>
